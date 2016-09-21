317be2d06931d7f9fa2b7567bb63677db456d542
fforcher <francesco.forcher@gmail.com>
2016-09-21 16:16:25 +0200

    Scritto un readme decente

--- SCRIPT_ROOT_ECLIPSE/eclipse_root606.sh
+++ SCRIPT_ROOT_ECLIPSE/eclipse_root606.sh
@@ -1,13 +1,12 @@
 #!/bin/zsh
 
-cd ~/ROOT/v5-34-36
+cd ~/ROOT/v6-06-08
 source ./bin/thisroot.sh
 cd
 
 # TODO switchare compiler, ma richiederebbe la password di root (il superuser...)
-notify-send "May need to set the compiler with root_534_select";
+notify-send "May need to set the compiler with root_606_select";
 notify-send "$(c++ --version)"
-
 
 # TODO fare simbolic link a /usr/local/include
 # echo "Linkando ~/ROOT/v5-34-36 a /usr/local/include/ROOT"
